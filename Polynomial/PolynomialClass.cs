c9d3928807c27c97ee7c5f150734c8e723e7fa16
Artem-gaus <gausin.artem@gmail.com>
2018-09-18 16:05:19 +0300

    Add project files.

---
+++ Polynomial/PolynomialClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    class PolynomialClass
+    {
+        protected int Degree {get; set;}
+        protected int[] Сoefficients;
+        public PolynomialClass(int degree, int[] coefficients)
+        {
+            this.Degree = degree;
+            this.Сoefficients = new int[coefficients.Length];
+            foreach (int i in coefficients)
+            {
+                this.Сoefficients[i] = coefficients[i];
+            }
+        }
+    }
+}
